59e8f094c209fb56ccd4dbc9b69cef266942b5ae
Unknown <deepikambigaij@squashapps.com>
2018-11-25 22:48:28 +0530

    Removed dist folders

--- server/dist/src/constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var constants = {
-    NOTIFICATION_URL: "http://localhost:8081",
-    REDIRECT_URL: "http://localhost:3000/login"
-};
-
-module.exports = constants;
--- server/dist/src/validation/schemas/registerSchema.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var Joi = require('joi');
-
-module.exports = Joi.object().keys({
-  email: Joi.string().email().min(3).max(50).required(),
-  password: Joi.string().min(6).max(80).required()
-}).strict();
